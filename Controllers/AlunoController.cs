7e9f47174249b7788ea2d8abde889bf5005dc91a
Nelson Neto <nelsonp2n@gmail.com>
2019-04-04 00:03:51 -0300

    Estudo API - CRUD efetuado diretamente em um arquivo JSON

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -13,11 +13,11 @@ namespace WebApp2.Controllers
         public AlunoController() { }
 
         // GET api/aluno
-        [HttpGet("Todos")]
+        [HttpGet]
         public ActionResult<IEnumerable<Aluno>> Gets()
         {
             Aluno aluno = new Aluno();
-            return aluno.listaAlunos();
+            return aluno.ListarAluno();
         }
 
         // GET api/aluno/5
@@ -25,19 +25,36 @@ namespace WebApp2.Controllers
         public ActionResult<Aluno> GetById(int id)
         {
             Aluno aluno = new Aluno();
-            return aluno.listaAlunos().Where(x => x.id == id).FirstOrDefault();
+            return aluno.ListarAluno().Where(x => x.id == id).FirstOrDefault();
         }
 
         // POST api/aluno
         [HttpPost("")]
-        public void Post([FromBody] string value) { }
+        public List<Aluno> Post([FromBody] Aluno Aluno) 
+        {
+            Aluno _aluno = new Aluno();
+
+            _aluno.Inserir(Aluno);
+
+            return _aluno.ListarAluno();
+        }
 
         // PUT api/aluno/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value) { }
+        public Aluno Put(int id, [FromBody] Aluno Aluno) 
+        { 
+            Aluno _aluno = new Aluno();
+
+           return _aluno.Atualizar(id, Aluno);
+        }
 
         // DELETE api/aluno/5
         [HttpDelete("{id}")]
-        public void DeleteById(int id) { }
+        public void DeleteById(int id) 
+        {
+            Aluno _aluno = new Aluno();
+
+            _aluno.Deletar(id);
+        }
     }
 }
--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
 
 namespace WebApp2.Models
 {
     public class Aluno
     {
-        
+        // private IHostingEnvironment _environment;
+
+        // public IHostingEnvironment environment { get; set; }
         public int id { get; set; }
         public string nome { get; set; }
         public string sobrenome { get; set; }
@@ -15,12 +20,76 @@ namespace WebApp2.Models
         public int ra { get; set; }
 
 
-        public List<Aluno> listaAlunos()
+        public List<Aluno> ListarAluno()
         {
 
-            var caminhoArquivo = @"~Base.json";
+            var caminhoArquivo = @"Base.json";
 
+            var json = File.ReadAllText(caminhoArquivo);
 
+            var listaAlunos = JsonConvert.DeserializeObject<List<Aluno>>(json);
+
+            return listaAlunos;
+        }
+
+        public bool ReescreverArquivo(List<Aluno> listaAlunos)
+        {
+            var caminhoArquivo =  @"Base.json";
+
+            var json = JsonConvert.SerializeObject(listaAlunos, Formatting.Indented);
+
+            File.WriteAllText(caminhoArquivo, json);
+
+            return true;
+        }
+
+        public Aluno Inserir(Aluno Aluno)
+        {
+            var listaAlunos = this.ListarAluno();
+            var maxId = listaAlunos.Max(aluno => aluno.id);
+            Aluno.id = maxId + 1;
+            listaAlunos.Add(Aluno);
+
+            ReescreverArquivo(listaAlunos);
+            return Aluno;
+        }
+
+        public Aluno Atualizar(int id, Aluno Aluno)
+        {
+
+            var listaAlunos = this.ListarAluno();
+
+            var itemIndex = listaAlunos.FindIndex(p => p.id == Aluno.id);
+
+            if(itemIndex >= 0)
+            {
+                Aluno.id = id;
+                listaAlunos[itemIndex] = Aluno;
+            } else
+            {
+                return null;
+            }
+
+            ReescreverArquivo(listaAlunos);
+            return Aluno;
+        }
+
+        public bool Deletar(int id)
+        {
+            var listaAlunos = this.ListarAluno();
+
+            var itemIndex = listaAlunos.FindIndex(p => p.id == id);
+
+            if(itemIndex >= 0)
+            {
+                listaAlunos.RemoveAt(itemIndex);
+            } else 
+            {
+                return false;
+            }
+
+            ReescreverArquivo(listaAlunos);
+            return true;
         }
     }
 }
